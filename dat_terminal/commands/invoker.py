9c5d57bfa16ed8a537268827f2cbc2ce74cdc1dc
Alpaca <paulocastrogalvao@hotmail.com>
2022-12-01 20:37:17 -0400

    Fixed aliases

--- dat_terminal/commands/invoker.py
+++ dat_terminal/commands/invoker.py
@@ -8,8 +8,11 @@ class CommandInvoker:
         self.command_list = {}
 
     def add_command(self, name: str, command: Command, aliases: tuple[str] = None) -> None:
-        for command_name in [*aliases, name]:
-            self.command_list[command_name] = command
+        if aliases:
+            for command_name in aliases:
+                self.command_list[command_name] = command
+        
+        self.command_list[name] = command
 
     def invoke_command(self, user_input: Input) -> None:
         if not user_input.command_identifier:
